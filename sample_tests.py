cc6fe83f2b78cffaa0c28571aa91a401db556ebd
akozlova <annakozlova92@gmail.com>
2022-08-23 13:08:58 -0700

    add more details to the description of the test

--- sample_tests.py
+++ sample_tests.py
@@ -3,15 +3,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# DESCRIBE mice information tracking app MiceTrack
 
-# DESCRIBE: Create new mouse with recent date
+
 def test_create_new_mouse():
+    """DESCRIBE: Create new mouse with most recent date"""
+
+    # it "confirms that new mice row can be added"
     browser = webdriver.Chrome()
     browser.get('http://localhost:5000/')
 
-    # add explicit wait to fix issue
-    # no such element: Unable to locate element: {"method":"css selector","selector":"[id="test-id-1"]
-    # wait untill button got rendered to click on it
     try:
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.ID, 'test-id-1'))
@@ -33,13 +34,14 @@ def test_create_new_mouse():
 
     success_message = 'The info has been added to the table'
 
-    # verify if there is the success message "The info has been added to the table"
     try:
         WebDriverWait(browser, 10).until(
             EC.presence_of_element_located((By.ID, 'msg-status'))
         )
         alert_status = browser.find_element(By.ID, 'msg-status')
-        # temp logging out this message to the console to verify that test passed
+
+        # verify if there is the success message "The info has been added to the table"
+        # temp logging out this message to the console to verify that test passed or failed
         if success_message in alert_status.text:
             print('PASS', success_message)
         else:
@@ -50,4 +52,7 @@ def test_create_new_mouse():
     browser.quit()
 
 
+# run tests
 test_create_new_mouse()
+
+# todo: reporting
